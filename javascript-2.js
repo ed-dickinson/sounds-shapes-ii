89017ff961b2e6d9b69260de99cc679dc2041d4e
ed-dickinson <edwardejd@gmail.com>
2022-06-17 12:15:45 +0100

    changed initial draw method

--- javascript-2.js
+++ javascript-2.js
@@ -38,38 +38,19 @@ const mouseUp = () => {
   click.created_shape = false
 }
 
-const hUpdater = (path) => {
-  let reverse_path = path.sort((a, b)=>{return b.x - a.x})
-  let path_i = 0
-  let path_count = path.filter(point => {return point !== undefined}).length
-  
-  reverse_path.forEach(point => {
-    let i2 = path_i < path_count / 2 ? path_i * 4 : (path_count - path_i - 1) * 4
-
-    let original_point = path.find(n=>{return n.x === point.x})
-    original_point.h = i2
-
-    let adjusted_y = point.y + point.h
-    path_i++
-  })
-}
-
 const mouseMove = () => {
 
   click.mousemoved = true
-
-  let time_at_this_point = new Date().getTime()
-  click.path[event.x] = {x: event.x, y: event.y, h: 0}
-  hUpdater(click.path)
+  // click.path.push({x: event.x, y: event.y})
+  click.path[event.x] = {x: event.x, y: event.y}
+  // console.log('mousemove:', event.x, event.y)
   current_shape.updateShape(click.path)
   if (!click.on_shape && !click.created_shape) {
 
     click.created_shape = true
   }
-  time_at_last_point = new Date().getTime()
-}
 
-let time_at_last_point = null
+}
 
 const mouseDown = () => {
   if (event.path[0].classList.contains('path')) { // if click on existing shape
@@ -80,8 +61,6 @@ const mouseDown = () => {
     current_shape = createShape()
     console.log(current_shape)
   }
-  time_at_last_point = new Date().getTime()
-  console.log(time_at_last_point)
   click.mousedown = { x: event.x, y: event.y }
   document.addEventListener('mousemove', mouseMove)
   document.addEventListener('mouseup', mouseUp)
@@ -104,14 +83,11 @@ function Shape(path) {
 
   this.updateShape = (path) => {
 
+    path.sort((a, b)=>{return a.x - b.x})
     this.path = path
-    console.log(path)
-
-    let sorted_path = path.sort((a, b)=>{return a.x - b.x})
-
     let path_string = ''
     let path_length = 0
-    sorted_path.forEach(point => {
+    path.forEach(point => {
       if (path_string === '') {
         path_string = `M ${point.x} ${point.y} `
       } else {
@@ -121,28 +97,13 @@ function Shape(path) {
     })
     let reverse_path = path.sort ((a, b)=>{return b.x - a.x})
     let path_i = 0
-    // reverse_path.forEach(point => {
-    //   let i2 = path_i < path_length / 2 ? path_i * 4 : (path_length - path_i - 1) * 4
-    //   console.log(this.path)
-    //
-    //   let original_point = path.find(n=>{return n.x === point.x})
-    //   original_point.h = i2
-    //
-    //   let adjusted_y = point.y + point.h
-    //   path_string += `L ${point.x} ${adjusted_y} `
-    //   path_i++
-    // })
     reverse_path.forEach(point => {
-      let adjusted_y = point.y + point.h
+      let i2 = path_i < path_length / 2 ? path_i * 4 : (path_length - path_i - 1) * 4
+      let adjusted_y = point.y + i2
       path_string += `L ${point.x} ${adjusted_y} `
       path_i++
     })
-    // reverse_path.forEach(point => {
-    //   let adjusted_y = point.y + point.h
-    //   path_string += `L ${point.x} ${adjusted_y} `
-    //   path_i++
-    // })
-    path_string += 'Z'
+    path += 'Z'
     this.dom.children[0].setAttribute('d', path_string)
   }
 }
